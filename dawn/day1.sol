c821a1cda7c347ff86b7801d3cc24a1e295ee7ae
joycefey <915604493@qq.com>
2026-03-02 18:50:28 +0800

    test

--- dawn/day1.sol
+++ dawn/day1.sol
@@ -1,1 +1,11 @@
-// SPDX-License-Identifer: MIT
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract clickcounter {
+    //
+    uint256 public counter;
+
+    //
+    function click()public {
+        counter++;
+    }
+}
